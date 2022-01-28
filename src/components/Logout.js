f06875181af6adc9a202772d62d8e50126e5eb07
Brett Smith <brettsmith212@gmail.com>
2022-01-28 10:04:55 -0800

    logout completed

--- src/components/Logout.js
+++ src/components/Logout.js
@@ -1,8 +1,22 @@
-import React from 'react';
+import React, { useEffect } from "react";
+import { useHistory } from "react-router-dom";
+import axiosWithAuth from "../utils/axiosWithAuth";
 
-const Logout = () => {        
-    return(<div></div>);
-}
+const Logout = () => {
+  const { push } = useHistory();
+
+  useEffect(() => {
+    axiosWithAuth()
+      .post("/logout")
+      .then((res) => {
+        localStorage.removeItem("token");
+        push("/login");
+      })
+      .catch((err) => console.log(err));
+  }, []);
+
+  return <div></div>;
+};
 
 export default Logout;
 
