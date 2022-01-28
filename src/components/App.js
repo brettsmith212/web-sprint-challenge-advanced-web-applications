9345d3047ca68fc6f2dd6c7982aca38ee18209b5
Brett Smith <brettsmith212@gmail.com>
2022-01-28 09:28:40 -0800

    route authentication complete

--- src/components/App.js
+++ src/components/App.js
@@ -7,6 +7,7 @@ import BloomHeader from "./BloomHeader";
 import Login from "./Login";
 import View from "./View";
 import Logout from "./Logout";
+import PrivateRoute from "./PrivateRoute";
 
 const App = () => {
   return (
@@ -20,12 +21,8 @@ const App = () => {
         <Route path="/login">
           <Login />
         </Route>
-        <Route path="/view">
-          <View />
-        </Route>
-        <Route path="/logout">
-          <Logout />
-        </Route>
+        <PrivateRoute path="/view" component={View} />
+        <PrivateRoute path="/logout" component={Logout} />
       </RouteContainer>
     </AppContainer>
   );
